5e77462f67ddd6e6802406246035b63b959d4254
Tom <tom@onyx.(none)>
2012-11-12 11:12:52 +1100

    - New build with complete output

--- src/au/edu/unimelb/rdr/RDFController.java
+++ src/au/edu/unimelb/rdr/RDFController.java
@@ -8,34 +8,49 @@ import com.hp.hpl.jena.sdb.Store;
 import com.hp.hpl.jena.util.FileManager;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 public class RDFController {
 
     private Model model;
     private FileManager fileManager;
+    private PrintStream printStream;
 
     public RDFController(Store store, String modelName) {
-        fileManager = FileManager.get();
-        model = SDBFactory.connectNamedModel(store, modelName);
+        this(store, modelName, System.out);
     }
 
-    public RDFController(ModelMaker mdlMaker, String modelName) {
-        model = mdlMaker.getModel(modelName);
+    public RDFController(Store store, String modelName, PrintStream printStream) {
+        fileManager = FileManager.get();
+        model = SDBFactory.connectNamedModel(store, modelName);
+        this.printStream = printStream;
+    }
+
+    public RDFController(ModelMaker modelMaker, String modelName) {
+        this(modelMaker, modelName, System.out);
+    }
+
+    public RDFController(ModelMaker modelMaker, String modelName, PrintStream printStream) {
+        model = modelMaker.getModel(modelName);
+        this.printStream = printStream;
     }
 
     public void add(String fileName) throws IOException {
-        InputStream is = fileManager.open(fileName);
-        System.out.println("Adding RDF to database...");
-        model.read(is, "", "N-TRIPLE");
-        is.close();
+        InputStream inputStream = fileManager.open(fileName);
+        printStream.println("Adding RDF to database...");
+        model.read(inputStream, "", "N-TRIPLE");
+        inputStream.close();
+        printStream.println("Action completed.");
     }
 
     public void remove(String fileName) throws IOException {
-        InputStream is = fileManager.open(fileName);
-        System.out.println("Removing RDF from database...");
+        InputStream inputStream = fileManager.open(fileName);
+        printStream.println("Removing RDF from database...");
         Model removeModel = ModelFactory.createDefaultModel();
-        removeModel.read(is, "", "N-TRIPLE");
+        removeModel.read(inputStream, "", "N-TRIPLE");
         model.remove(removeModel);
-        is.close();
+        inputStream.close();
+        printStream.println("Action completed.");
     }
 }
