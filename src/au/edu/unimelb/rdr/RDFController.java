5729469d41724707b541e33eb29d6b982236c6f2
Tom <tom@onyx.(none)>
2012-11-20 13:14:55 +1100

    - Restructured main and controller functions/classes

--- src/au/edu/unimelb/rdr/RDFController.java
+++ src/au/edu/unimelb/rdr/RDFController.java
@@ -7,8 +7,9 @@ import com.hp.hpl.jena.query.Syntax;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.ModelMaker;
+import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.RDFNode;
-import com.hp.hpl.jena.rdf.model.ResourceFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.sdb.SDBFactory;
 import com.hp.hpl.jena.sdb.Store;
 import com.hp.hpl.jena.util.FileManager;
@@ -23,33 +24,33 @@ import org.apache.commons.logging.LogFactory;
 
 public class RDFController {
 
-    private Model model;
+    private Model remoteModel;
+    private Model localModel;
     private FileManager fileManager;
     private PrintStream printStream;
-    private Store store;
     private Log log = LogFactory.getLog(RDFController.class);
-    private static String UPSTREAM_MODEL = "";
-    private static String KEEP_MODEL = "";
-    private static String VIVO_MODEL = "http://vitro.mannlib.cornell.edu/default/vitro-kb-2";
+    //"http://vitro.mannlib.cornell.edu/default/vitro-kb-2";
     private static String TYPE = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
 
-    public RDFController(Store store, String modelName) {
-        this(store, modelName, System.out);
+    public RDFController(Store store, String remoteModelName, String localModelName) {
+        this(store, null, remoteModelName, localModelName, System.out);
     }
 
-    public RDFController(Store store, String modelName, PrintStream printStream) {
-        fileManager = FileManager.get();
-        model = SDBFactory.connectNamedModel(store, modelName);
-        this.store = store;
-        this.printStream = printStream;
+    public RDFController(ModelMaker modelMaker, String remoteModelName, String localModelName) {
+        this(null, modelMaker, remoteModelName, localModelName, System.out);
     }
 
-    public RDFController(ModelMaker modelMaker, String modelName) {
-        this(modelMaker, modelName, System.out);
-    }
-
-    public RDFController(ModelMaker modelMaker, String modelName, PrintStream printStream) {
-        model = modelMaker.getModel(modelName);
+    public RDFController(Store store, ModelMaker modelMaker, String remoteModelName, String localModelName, PrintStream printStream) {
+        if (store != null) {
+            this.remoteModel = SDBFactory.connectNamedModel(store, remoteModelName);
+            this.localModel = SDBFactory.connectNamedModel(store, localModelName);
+        } else if (modelMaker != null) {
+            this.remoteModel = modelMaker.getModel(remoteModelName);
+            this.localModel = modelMaker.getModel(localModelName);
+        } else {
+            throw new IllegalArgumentException("Store or ModelMaker must be provided");
+        }
+        this.fileManager = FileManager.get();
         this.printStream = printStream;
     }
 
@@ -57,11 +58,11 @@ public class RDFController {
         InputStream inputStream = fileManager.open(fileName);
         log("Adding RDF to database...");
         long startTime = System.currentTimeMillis();
-        long startSize = model.size();
-        model.read(inputStream, "", "N-TRIPLE");
+        long startSize = remoteModel.size();
+        remoteModel.read(inputStream, "", "N-TRIPLE");
         inputStream.close();
         long endTime = System.currentTimeMillis();
-        long endSize = model.size();
+        long endSize = remoteModel.size();
         long duration = endTime - startTime;
         long sizeDelta = endSize - startSize;
         log("Action completed [" + duration + "ms, " + sizeDelta + " records]");
@@ -71,109 +72,101 @@ public class RDFController {
         InputStream inputStream = fileManager.open(fileName);
         log("Removing RDF from database...");
         long startTime = System.currentTimeMillis();
-        long startSize = model.size();
+        long startSize = remoteModel.size();
         Model removeModel = ModelFactory.createDefaultModel();
         removeModel.read(inputStream, "", "N-TRIPLE");
-        model.remove(removeModel);
+        remoteModel.remove(removeModel);
         inputStream.close();
         long endTime = System.currentTimeMillis();
         long duration = endTime - startTime;
-        long endSize = model.size();
+        long endSize = remoteModel.size();
         long sizeDelta = endSize - startSize;
         log("Action completed [" + duration + "ms, " + sizeDelta + " records]");
     }
 
-    public void process(String addFilename, String delFilename) {
-        InputStream addStream = fileManager.open(addFilename);
-        InputStream delStream = fileManager.open(delFilename);
+    public void process(String addFilename, String delFilename) throws IOException {
+        /* Temporary models used in processing */
+        Model addModel = null;
+        Model delModel = null;
 
-        Model addModel = ModelFactory.createDefaultModel();
-        addModel.read(addStream, "", "N-TRIPLE");
-        addStream.close();
+        if (addFilename != null) {
+            addModel = loadModelFromFile(addFilename);
+        }
+        if (delFilename != null) {
+            delModel = loadModelFromFile(delFilename);
+        }
 
-        Model delModel = ModelFactory.createDefaultModel();
-        delModel.read(delStream, "", "N-TRIPLE");
-        delStream.close();
+        if (addModel != null) {
+            List<QuerySolution> solutionList;
+            Model typesModel;
 
-        String typesQuery = "CONSTRUCT ?s ?p ?o"
-                + "WHERE"
-                + "{"
-                + "BIND(" + TYPE + " as ?p)"
-                + "?s ?p ?o"
-                + "}";
+            log("Adding " + addFilename + " to remote model");
+            remoteModel.add(addModel);
 
-        Model typesModel = QueryExecutionFactory.create(typesQuery, faeModel).execConstruct();
+            String typesQuery = "CONSTRUCT { ?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?o }\n"
+                    + "WHERE\n"
+                    + "{\n"
+                    + "?s <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?o\n"
+                    + "}";
+            QueryExecution qe = QueryExecutionFactory.create(typesQuery, Syntax.syntaxARQ, remoteModel);
+            typesModel = qe.execConstruct();
 
+            String query = "SELECT DISTINCT ?o WHERE { ?s ?p ?o }";
 
+            solutionList = results(query, localModel);
 
-        /*Step 1:
-         Add ADD to FAE*/
-
-        model.add(addModel);
-
-        /*Step 2:
-         For each assertion (S, P, O) in RDR:
-         * */
-        String query = "SELECT DISTINCT ?o WHERE { ?s ?p ?o }";
-        List<QuerySolution> solutionList;
-
-        solutionList = runQuery(query, model);
-
-        for (QuerySolution solution : solutionList) {
-            RDFNode node = solution.get("?o");
-            if (node.isResource()) {
-                String nodeURI = node.asResource().getURI();
-                String referredToSubjectQuery = "SELECT ?p ?o WHERE {" + nodeURI + " ?p ?o}";
-                String referredToObjectQuery = "SELECT ?s ?p WHERE {?s ?p " + nodeURI + "}";
-                if (runQuerySize(referredToSubjectQuery, addModel) == 0) {
-                    if (runQuerySize(referredToObjectQuery, addModel) == 0) {
-                        /*O is not referred to in ADD*/
-                        if (runQuerySize(referredToSubjectQuery, delModel) > 0
-                                || runQuerySize(referredToObjectQuery, delModel) > 0) {
-                            /*O is referred to in DEL*/
-                            String typeAssertionQuery = "SELECT ?type WHERE {" + nodeURI + " " + TYPE + " ?type}";
+            for (QuerySolution solution : solutionList) {
+                RDFNode node = solution.get("?o");
+                if (node.isResource()) {
+                    String nodeURI = node.asResource().getURI();
+                    String referredToSubjectQuery = "SELECT ?p ?o WHERE {<" + nodeURI + "> ?p ?o}";
+                    String referredToObjectQuery = "SELECT ?s ?p WHERE {?s ?p <" + nodeURI + ">}";
+                    if (resultsSize(referredToSubjectQuery, delModel) > 0 || resultsSize(referredToObjectQuery, delModel) > 0) {
+                        /* O is referred to in DEL */
+                        if (resultsSize(referredToSubjectQuery, addModel) == 0 && resultsSize(referredToObjectQuery, addModel) == 0) {
+                            /* O is not referred to in ADD */
+                            String typeAssertionQuery = "SELECT ?type WHERE {<" + nodeURI + "> <" + TYPE + "> ?type}";
                             List<String> typeSolutionList;
 
                             typeSolutionList = getQueryAttribute(typeAssertionQuery, delModel, "?type");
                             if (!typeSolutionList.isEmpty()) {
-
-                                /*Assertion O <rfd:type> T exists in DEL */
+                                /* Assertion O <rfd:type> T exists in DEL */
                                 List<String> otherTypeSolutionList;
 
                                 otherTypeSolutionList = getQueryAttribute(typeAssertionQuery, typesModel, "?type");
                                 if (otherTypeSolutionList.containsAll(typeSolutionList) && otherTypeSolutionList.size() == typeSolutionList.size()) {
                                     /* Assertion O <rdf:type> T' (where T' != T) does not exist in TYPES */
+                                    Model childModel = childModel(node, delModel, null);
+                                    log("Adding child model for " + nodeURI + " to local model, " + childModel.size() + " assertions");
+                                    localModel.add(childModel);
                                 }
                             }
                         }
                     }
+                } else if (node.isAnon()) {
+                    log("Found anon resource: " + node.toString());
                 }
             }
-            /*
-             If
-
-             consider this a deletion, determine children and add this into RDR
-             Else
-             consider this an edit of a property/subproperty and continue*/
-            System.out.println(solution.toString());
-
         }
 
+        if (delModel != null) {
+            log("Deleting " + delFilename + " from remote model");
+            remoteModel.remove(delModel);
+        }
 
+        addModel.close();
+        delModel.close();
+    }
 
-        /*Step 3:
-         Delete DEL from FAE*/
-        model.remove(delModel);
-
-
-        log("Adding RDF to database...");
-
+    public void close() {
+        this.localModel.close();
+        this.remoteModel.close();
     }
 
     private List<String> getQueryAttribute(String query, Model model, String attribute) {
         List<QuerySolution> solutionList;
 
-        solutionList = runQuery(query, model);
+        solutionList = results(query, model);
         List<String> valueList;
 
         valueList = new ArrayList<String>(solutionList.size());
@@ -183,11 +176,11 @@ public class RDFController {
         return valueList;
     }
 
-    private int runQuerySize(String query, Model model) {
-        return runQuery(query, model).size();
+    private int resultsSize(String query, Model model) {
+        return results(query, model).size();
     }
 
-    private List<QuerySolution> runQuery(String query, Model model) {
+    private List<QuerySolution> results(String query, Model model) {
         QueryExecution qexec;
         List<QuerySolution> solutionList;
         Iterator<QuerySolution> rs;
@@ -202,26 +195,57 @@ public class RDFController {
         return solutionList;
     }
 
-    public void pruneModel(Model upstreamModel) {
-        Model vivoModel = SDBFactory.connectNamedModel(store, VIVO_MODEL);
-        Model keepModel = SDBFactory.connectNamedModel(store, KEEP_MODEL);
-        String query = "SELECT DISTINCT ?s WHERE { ?s ?p ?o }";
-        QueryExecution qexec = QueryExecutionFactory.create(query, upstreamModel);
-        try {
-
-            Iterator<QuerySolution> rs = qexec.execSelect();
-            for (; rs.hasNext();) {
-                QuerySolution soln = rs.next();
-                System.out.println(soln.toString());
-
-            }
-        } finally {
-            qexec.close();
-        }
-    }
-
     private void log(String output) {
         printStream.println(output);
         log.info(output);
+    }
+
+    private Model loadModelFromFile(String filename) throws IOException {
+        Model fileModel;
+        InputStream fileStream;
+
+        fileStream = fileManager.open(filename);
+        if (fileStream == null) {
+            throw new IOException("Unable to open file: " + filename);
+        }
+        fileModel = ModelFactory.createDefaultModel();
+        fileModel.read(fileStream, "", "N-TRIPLE");
+        try {
+            fileStream.close();
+        } catch (IOException ioe) {
+            log("Error closing stream: " + ioe.getMessage());
+        }
+        return fileModel;
+    }
+
+    private Model childModel(RDFNode subjectNode, Model originModel, Model childModel) {
+        if (childModel == null) {
+            childModel = ModelFactory.createDefaultModel();
+        }
+
+        Resource subject = subjectNode.asResource();
+        String subjectURI = subject.getURI();
+        String referredToSubjectQuery = "SELECT ?p ?o WHERE {<" + subjectURI + "> ?p ?o}";
+
+        for (QuerySolution solution : results(referredToSubjectQuery, originModel)) {
+            RDFNode propertyNode;
+            RDFNode objectNode;
+            Property property;
+
+            propertyNode = solution.get("?p");
+            property = childModel.createProperty(propertyNode.asResource().getURI());
+            objectNode = solution.get("?o");
+            childModel.add(subject, property, objectNode);
+            if (objectNode.isResource()) {
+                if (childModel.contains(objectNode.asResource(), null)) {
+                    /* The object of this result has already been processed as a
+                     *  subject so continue
+                     */
+                    continue;
+                }
+                childModel = childModel(objectNode, originModel, childModel);
+            }
+        }
+        return childModel;
     }
 }
--- src/au/edu/unimelb/rdr/VivoIngest.java
+++ src/au/edu/unimelb/rdr/VivoIngest.java
@@ -2,66 +2,61 @@ package au.edu.unimelb.rdr;
 
 import au.edu.unimelb.rdr.database.DatabaseConnection;
 import au.edu.unimelb.rdr.database.SDBDatabaseConnection;
+import com.hp.hpl.jena.ontology.impl.OntModelImpl;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.Syntax;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.sdb.SDBFactory;
 import com.hp.hpl.jena.sdb.Store;
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.Statement;
+import java.io.IOException;
+import java.util.Iterator;
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class VivoIngest {
+
+    private static Log log = LogFactory.getLog(RDFController.class);
 
     private static CommandLine parseOptions(String[] args) throws ParseException {
         Options options = new Options();
         options.addOption(OptionBuilder.hasArg(true).isRequired(true).withDescription("Database user name").create("userName"));
         options.addOption(OptionBuilder.hasArg(true).isRequired(true).withDescription("Database password").create("password"));
         options.addOption(OptionBuilder.hasArg(true).isRequired(true).withDescription("Database string").create("dbString"));
-        options.addOption(OptionBuilder.hasArg(true).isRequired(false).withDescription("TTL filename").create("fileName"));
-        options.addOption(OptionBuilder.hasArg(true).isRequired(false).withDescription("JENA type").create("jenaType"));
+        options.addOption(OptionBuilder.hasArg(true).isRequired(true).withDescription("Remote model name").create("remoteModelName"));
+        options.addOption(OptionBuilder.hasArg(true).isRequired(true).withDescription("Local model name").create("localModelName"));
+        options.addOption(OptionBuilder.hasArg(true).isRequired(false).withDescription("Add filename").create("addFileName"));
+        options.addOption(OptionBuilder.hasArg(true).isRequired(false).withDescription("Delete filename").create("delFileName"));
         options.addOption(OptionBuilder.hasArg(true).isRequired(false).withDescription("Database type").create("dbType"));
-        options.addOption(OptionBuilder.hasArg(true).isRequired(false).withDescription("Add data?").create("addData"));
-        options.addOption(OptionBuilder.hasArg(true).isRequired(false).withDescription("Model name").create("modelName"));
-        options.addOption(OptionBuilder.hasArg(true).isRequired(false).withDescription("Drop tables?").create("dropTables"));
         options.addOption(OptionBuilder.hasArg(false).isRequired(false).withDescription("Show help").create("h"));
         CommandLineParser parser = new BasicParser();
         CommandLine cmd = parser.parse(options, args);
         if (cmd.hasOption("h")) {
             consoleHelp();
         }
-        if (!cmd.hasOption("dropTables") && !cmd.hasOption("fileName")) {
-            System.out.println("Missing required option: fileName");
-            consoleHelp();
-        }
         return cmd;
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         CommandLine cmd = null;
         try {
             cmd = parseOptions(args);
         } catch (ParseException pe) {
             System.out.println(pe.getMessage());
             consoleHelp();
+            return;
         }
 
-        String jenaType = null;
         String dbType = null;
-
-        Boolean addData = null;
-
-        String modelName = null;
-
-        Boolean dropTables = null;
-
-        if (cmd.hasOption("jenaType")) {
-            jenaType = cmd.getOptionValue("jenaType");
-        } else {
-            jenaType = "SDB";
-        }
 
         if (cmd.hasOption("dbType")) {
             dbType = cmd.getOptionValue("dbType");
@@ -69,88 +64,152 @@ public class VivoIngest {
             dbType = "MySQL";
         }
 
-        if (cmd.hasOption("addData")) {
-            addData = Boolean.parseBoolean(cmd.getOptionValue("addData"));
-        } else {
-            addData = Boolean.TRUE;
-        }
-
-        if (cmd.hasOption("modelName")) {
-            modelName = cmd.getOptionValue("modelName");
-        } else {
-            modelName = "http://vitro.mannlib.cornell.edu/default/vitro-kb-2";
-        }
-
-        if (cmd.hasOption("dropTables")) {
-            dropTables = Boolean.parseBoolean(cmd.getOptionValue("dropTables"));
-        } else {
-            dropTables = Boolean.FALSE;
-        }
-
+        String remoteModelName = cmd.getOptionValue("remoteModelName");
+        String localModelName = cmd.getOptionValue("localModelName");
         String password = cmd.getOptionValue("password");
         String dbString = cmd.getOptionValue("dbString");
-        String fileName = cmd.getOptionValue("fileName");
+        String addFileName = cmd.getOptionValue("addFileName");
+        String delFileName = cmd.getOptionValue("delFileName");
         String userName = cmd.getOptionValue("userName");
 
-        if (dropTables) {
-            Connection conn = null;
-
-            if (jenaType.equalsIgnoreCase("SDB")) {
-                SDBDatabaseConnection sdc = new SDBDatabaseConnection(userName, password, dbString, dbType);
-                conn = sdc.getConnection();
-            } else {
-                DatabaseConnection dbc = new DatabaseConnection(userName, password, dbString, dbType);
-                conn = dbc.getConnection();
-            }
-
-            if (dbType.equalsIgnoreCase("Oracle")) {
-                String query = "BEGIN FOR i IN (SELECT table_name FROM user_tables) LOOP EXECUTE IMMEDIATE('DROP TABLE ' || user || '.' || i.table_name || ' CASCADE CONSTRAINTS'); END LOOP; END;";
-
-                CallableStatement callStmt = conn.prepareCall(query);
-                callStmt.execute();
-            } else {
-                String query = "SELECT CONCAT( \"DROP TABLE \",GROUP_CONCAT(TABLE_NAME)) AS stmt FROM information_schema.TABLES";
-
-                Statement statement = conn.createStatement();
-                statement.executeQuery(query);
-            }
-
-        } else {
-            RDFController controller;
-            if (jenaType.equals("SDB")) {
-                SDBDatabaseConnection sdc = new SDBDatabaseConnection(userName, password, dbString, dbType);
-                Store store = sdc.getStore();
-                controller = new RDFController(store, modelName);
-            } else {
-                DatabaseConnection dbc = new DatabaseConnection(userName, password, dbString, dbType);
-                controller = new RDFController(dbc.maker, modelName);
-            }
-            if (addData) {
-                controller.add(fileName);
-            } else {
-                controller.remove(fileName);
-            }
-        }
+        RDFController controller;
+        SDBDatabaseConnection sdc = new SDBDatabaseConnection(userName, password, dbString, dbType);
+        Store store = sdc.getStore();
+        controller = new RDFController(store, remoteModelName, localModelName);
+        controller.process(addFileName, delFileName);
+        controller.close();
     }
 
+    /*public static void main(String[] args) {
+     CommandLine cmd = null;
+     try {
+     cmd = parseOptions(args);
+     } catch (ParseException pe) {
+     System.out.println(pe.getMessage());
+     consoleHelp();
+     }
+
+     String jenaType = null;
+     String dbType = null;
+
+     Boolean addData = null;
+
+     String modelName = null;
+
+     Boolean dropTables = null;
+
+     if (cmd.hasOption("jenaType")) {
+     jenaType = cmd.getOptionValue("jenaType");
+     } else {
+     jenaType = "SDB";
+     }
+
+     if (cmd.hasOption("dbType")) {
+     dbType = cmd.getOptionValue("dbType");
+     } else {
+     dbType = "MySQL";
+     }
+
+     if (cmd.hasOption("addData")) {
+     addData = Boolean.parseBoolean(cmd.getOptionValue("addData"));
+     } else {
+     addData = Boolean.TRUE;
+     }
+
+     if (cmd.hasOption("modelName")) {
+     modelName = cmd.getOptionValue("modelName");
+     } else {
+     modelName = "http://vitro.mannlib.cornell.edu/default/vitro-kb-2";
+     }
+
+     if (cmd.hasOption("dropTables")) {
+     dropTables = Boolean.parseBoolean(cmd.getOptionValue("dropTables"));
+     } else {
+     dropTables = Boolean.FALSE;
+     }
+
+     String password = cmd.getOptionValue("password");
+     String dbString = cmd.getOptionValue("dbString");
+     String fileName = cmd.getOptionValue("fileName");
+     String userName = cmd.getOptionValue("userName");
+
+     if (dropTables) {
+     Connection conn = null;
+
+     if (jenaType.equalsIgnoreCase("SDB")) {
+     SDBDatabaseConnection sdc = new SDBDatabaseConnection(userName, password, dbString, dbType);
+     conn = sdc.getConnection();
+     } else {
+     DatabaseConnection dbc = new DatabaseConnection(userName, password, dbString, dbType);
+     try {
+     conn = dbc.getConnection();
+     } catch (SQLException sqle) {
+     error("Cannot create database connection: " + sqle.toString());
+     return;
+     }
+     }
+     try {
+     if (dbType.equalsIgnoreCase("Oracle")) {
+     String query = "BEGIN FOR i IN (SELECT table_name FROM user_tables) LOOP EXECUTE IMMEDIATE('DROP TABLE ' || user || '.' || i.table_name || ' CASCADE CONSTRAINTS'); END LOOP; END;";
+
+     CallableStatement callStmt = conn.prepareCall(query);
+     callStmt.execute();
+     } else {
+     String query = "SELECT CONCAT( \"DROP TABLE \",GROUP_CONCAT(TABLE_NAME)) AS stmt FROM information_schema.TABLES";
+
+     Statement statement = conn.createStatement();
+     statement.executeQuery(query);
+     }
+     } catch (SQLException sqle) {
+     error("Cannot drop tables: " + sqle.toString());
+     return;
+     }
+
+     } else {
+     RDFController controller;
+     if (jenaType.equals("SDB")) {
+     SDBDatabaseConnection sdc = new SDBDatabaseConnection(userName, password, dbString, dbType);
+     Store store = sdc.getStore();
+     controller = new RDFController(store, modelName);
+     } else {
+     DatabaseConnection dbc = new DatabaseConnection(userName, password, dbString, dbType);
+     controller = new RDFController(dbc.maker, modelName);
+     }
+     try {
+     if (addData) {
+     controller.add(fileName);
+     } else {
+     controller.remove(fileName);
+     }
+     } catch (IOException ioe) {
+     error("Can't perform RDF operation: " + ioe.getMessage());
+     return;
+     }
+     }
+     }*/
     private static void consoleHelp() {
         System.out.print("Correct usage of this application: ");
         System.out.println("java -jar VivoIngest.jar <flags>\n");
-        System.out.println("If a Jena type isn't chosen, this program will use SDB as the default.");
         System.out.println("If a database type isn't chosen, this program will use MySQL as the default.");
-        System.out.println("If adding or removing data isn't specified, this program will use adding data as the default.");
-        System.out.println("If a model name isn't chosen, this program will use kb-2 as default (the main VIVO model).\n");
-        System.out.println("If -dropTables is used, only the username, password and dbString are required (And optionally, the jenaType and dbType).\n");
         System.out.println("\n\tFlags:");
-        System.out.println("\n\t-jenaType SDB/RDB");
-        System.out.println("\t-userName johnDoe");
+        System.out.println("\n\t-userName johnDoe");
         System.out.println("\t-password secret123");
         System.out.println("\t-dbString jdbc:mysql://localhost/vivo");
         System.out.println("\t-dbType MySQL/Oracle");
-        System.out.println("\t-fileName bigfile6");
-        System.out.println("\t-addData true/false");
-        System.out.println("\t-modelName MODEL1");
-        System.out.println("\t-dropTables");
+        System.out.println("\t-addFileName add.ttl");
+        System.out.println("\t-addFileName del.ttl");
+        System.out.println("\t-remoteModelName remote-model");
+        System.out.println("\t-localModelName local-model");
         System.exit(0);
+    }
+
+    private static void info(String output) {
+        System.out.println(output);
+        log.info(output);
+    }
+
+    private static void error(String output) {
+        System.out.println(output);
+        log.error(output);
     }
 }
