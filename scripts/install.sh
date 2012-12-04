2cbad2af3bda9b71a74ae0430d4a8049f4be0df2
Tom <tom@onyx.(none)>
2012-12-04 13:56:29 +1100

    - Update installer to create log directory

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,16 +7,21 @@ USER_GROUP=rdr-unimelb
 USER_SHELL=/bin/false
 
 BIN_DIR=${USER_HOME}/bin
+LOG_DIR=/var/log/rdr-unimelb
 
 echo "Adding user to system..."
 groupadd ${USER_GROUP}
 useradd -m -d ${USER_HOME} -g ${USER_GROUP} -s ${USER_SHELL} ${USER_NAME}
 
 echo "Copying binaries to install location..."
-mkdir ${BIN_DIR}
+mkdir -p ${BIN_DIR}
 cp -rv dist/* ${BIN_DIR}
 chown -R ${USER_NAME}:${USER_GROUP} ${BIN_DIR}
 
 echo "Installing crontab..."
 cp scripts/crontab ${CRON_DIR}/rdr-unimelb
 sed -i -e "s#%%USER_NAME%%#${USER_NAME}#g" -e "s#%%BIN_DIR%%#${BIN_DIR}#g" ${CRON_DIR}/rdr-unimelb
+
+echo "Creating log directory..."
+mkdir -p ${LOG_DIR}
+chown -R ${USER_NAME}:${USER_GROUP} ${LOG_DIR}
