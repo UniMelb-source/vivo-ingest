79df7270421ae9bf662b76873d6bada9eaa448b8
Tom <tom@onyx.(none)>
2013-01-16 10:44:17 +1100

    - Update ingest script to process properly if only one of the ADD/DEL delta pair can be fetched

--- dist/ingest.sh
+++ dist/ingest.sh
@@ -17,24 +17,44 @@ LATEST_TS=$((${LATEST_TS} + ${DAYSEC}))
 while [ ${LATEST_TS} -lt ${NOW} ]
 do
 	CURRENT_DATE=$(date -d @${LATEST_TS} +%Y%m%d)
+
 	echo "Processing ${CURRENT_DATE}"
 	ADD_FILE=$(date -d @${LATEST_TS} +%Y%m%d)_ADD.ttl
 	ADD_URL=${URL_BASE}${ADD_FILE}
 	DEL_FILE=$(date -d @${LATEST_TS} +%Y%m%d)_DEL.ttl
 	DEL_URL=${URL_BASE}${DEL_FILE}
 	INGEST_ARGS="-action process -addFileName ${ADD_FILE} -removeFileName ${DEL_FILE} -jenaType ${DB_JENATYPE} -dbString ${DB_STRING} -userName ${DB_USERNAME} -password ${DB_PASSWORD} -localModelName ${DB_LOCAL_MODEL} -remoteModelName ${DB_REMOTE_MODEL}"
-	echo "Fetching TTL files"
-	wget -cnv ${ADD_URL} && wget -cnv ${DEL_URL}
-	RETURN=$?
 	echo ${CURRENT_DATE} > ${FILE_LATEST}
 	LATEST_TS=$((${LATEST_TS} + ${DAYSEC}))
-	if [ ${RETURN} -gt 0 ]
+
+	echo "Fetching add files"
+	wget -cnv ${ADD_URL}
+	ADD_RETURN=$?
+
+    echo "Fetching del file"
+    wget -cnv ${DEL_URL}
+	DEL_RETURN=$?
+
+	if [ ${ADD_RETURN} -gt 0 -a ${DEL_RETURN} -gt 0 ]
 	then
-		echo "Unable to fetch files: ${RETURN}"
+		echo "Unable to fetch files"
 		continue
 	fi
-	clean_up_file ${ADD_FILE}
-	clean_up_file ${DEL_FILE}
+
+    if [ ${ADD_RETURN} -gt 0 ]
+    then
+        touch ${ADD_FILE}
+    else
+	    clean_up_file ${ADD_FILE}
+    fi
+
+    if [ ${DEL_RETURN} -gt 0 ]
+    then
+        touch ${DEL_FILE}
+    else
+	    clean_up_file ${DEL_FILE}
+    fi
+
 	echo "Running ingest"
 	java ${JAVA_ARGS} -jar VivoIngest.jar ${INGEST_ARGS}
 	mv children.ttl ${CURRENT_DATE}-children.ttl
