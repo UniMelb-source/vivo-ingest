09baacb7e10b8813f3c288ec2bbe88a4f9462ea1
root <root@nsp-precise.vm.nsp.nectar.org.au>
2013-02-25 11:47:11 +1100

    - Fix issue with absolute path used when fetching deltas

--- scripts/ingest.sh
+++ scripts/ingest.sh
@@ -21,42 +21,46 @@ do
 	CURRENT_DATE=$(date -d @${LATEST_TS} +%Y%m%d)
 
 	echo "Processing ${CURRENT_DATE}"
-	ADD_FILE=${WORKING_DIR}/$(date -d @${LATEST_TS} +%Y%m%d)_ADD.ttl
+	ADD_FILE=$(date -d @${LATEST_TS} +%Y%m%d)_ADD.ttl
+	ADD_ABS_FILE=${WORKING_DIR}/${ADD_FILE}
 	ADD_URL=${URL_BASE}${ADD_FILE}
-	DEL_FILE=${WORKING_DIR}/$(date -d @${LATEST_TS} +%Y%m%d)_DEL.ttl
+
+	DEL_FILE=$(date -d @${LATEST_TS} +%Y%m%d)_DEL.ttl
+	DEL_ABS_FILE=${WORKING_DIR}/${DEL_FILE}
 	DEL_URL=${URL_BASE}${DEL_FILE}
-	INGEST_ARGS="-action process -addFileName ${ADD_FILE} -removeFileName ${DEL_FILE} -jenaType ${DB_JENATYPE} -dbString ${DB_STRING} -userName ${DB_USERNAME} -password ${DB_PASSWORD} -localModelName ${DB_LOCAL_MODEL} -remoteModelName ${DB_REMOTE_MODEL}"
+
+	INGEST_ARGS="-action process -addFileName ${ADD_ABS_FILE} -removeFileName ${DEL_ABS_FILE} -jenaType ${DB_JENATYPE} -dbString ${DB_STRING} -userName ${DB_USERNAME} -password ${DB_PASSWORD} -localModelName ${DB_LOCAL_MODEL} -remoteModelName ${DB_REMOTE_MODEL}"
 	echo ${CURRENT_DATE} > ${FILE_LATEST}
 	LATEST_TS=$((${LATEST_TS} + ${DAYSEC}))
 
 	echo "Fetching add files"
-	wget -cnv ${ADD_URL} -O ${ADD_FILE}
+	wget -cnv ${ADD_URL} -O ${ADD_ABS_FILE}
 	ADD_RETURN=$?
 
 	echo "Fetching del file"
-	wget -cnv ${DEL_URL} -O ${DEL_FILE}
+	wget -cnv ${DEL_URL} -O ${DEL_ABS_FILE}
 	DEL_RETURN=$?
 
 	if [ ${ADD_RETURN} -gt 0 -a ${DEL_RETURN} -gt 0 ]
 	then
 		echo "Unable to fetch files"
-		rm -f ${ADD_FILE}
-		rm -f ${DEL_FILE}
+		rm -f ${ADD_ABS_FILE}
+		rm -f ${DEL_ABS_FILE}
 		continue
 	fi
 
 	if [ ${ADD_RETURN} -gt 0 ]
 	then
-		touch ${ADD_FILE}
+		touch ${ADD_ABS_FILE}
 	else
-		clean_up_file ${ADD_FILE}
+		clean_up_file ${ADD_ABS_FILE}
 	fi
 
 	if [ ${DEL_RETURN} -gt 0 ]
 	then
-		touch ${DEL_FILE}
+		touch ${DEL_ABS_FILE}
 	else
-		clean_up_file ${DEL_FILE}
+		clean_up_file ${DEL_ABS_FILE}
 	fi
 
 	echo "Running ingest"
@@ -65,6 +69,6 @@ do
 	mv construct-add.ttl ${WORKING_DIR}/${CURRENT_DATE}-construct-add.ttl
 	mv construct-remove.ttl ${WORKING_DIR}/${CURRENT_DATE}-construct-remove.ttl
 	echo "Ingest done"
-	bzip2 ${ADD_FILE}
-	bzip2 ${DEL_FILE}
+	bzip2 ${ADD_ABS_FILE}
+	bzip2 ${DEL_ABS_FILE}
 done
