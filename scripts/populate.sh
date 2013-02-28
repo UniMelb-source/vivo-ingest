93cd2bfb181c9a2324e5e9b3ab461a077dbfaf87
Thomas Sullivan <tom@nsp-precise.vm.nsp.nectar.org.au>
2013-03-01 10:02:23 +1100

    - Update initial population script to operate in the working directory

--- scripts/populate.sh
+++ scripts/populate.sh
@@ -7,54 +7,61 @@ FILE_LATEST="ingest.latest"
 DAYSEC=86400
 URL_BASE="http://www.findanexpert.unimelb.edu.au/vivoDeltas/"
 
-JAVA_ARGS="-Xmx2048m" 
+JAVA_ARGS="-Xmx3072m" 
 
 NOW=$(date +%s)
 LATEST_DATE=$(cat ${FILE_LATEST})
 LATEST_TS=$(date -d ${LATEST_DATE} +%s)
 LATEST_TS=$((${LATEST_TS} + ${DAYSEC}))
 
+WORKING_DIR=/var/lib/rdr-unimelb/ingest
+
 while [ ${LATEST_TS} -lt ${NOW} ]
 do
 	CURRENT_DATE=$(date -d @${LATEST_TS} +%Y%m%d)
 
 	echo "Processing ${CURRENT_DATE}"
+
 	ADD_FILE=$(date -d @${LATEST_TS} +%Y%m%d)_ADD.ttl
+	ADD_ABS_FILE=${WORKING_DIR}/${ADD_FILE}
 	ADD_URL=${URL_BASE}${ADD_FILE}
+
 	DEL_FILE=$(date -d @${LATEST_TS} +%Y%m%d)_DEL.ttl
+	DEL_ABS_FILE=${WORKING_DIR}/${DEL_FILE}
 	DEL_URL=${URL_BASE}${DEL_FILE}
-	POPULATE_ARGS="-action add -addFileName ${ADD_FILE} -dbString ${DB_STRING} -localModelName ${DB_REMOTE_MODEL} -password ${DB_PASSWORD} -userName ${DB_USERNAME} -jenaType ${DB_JENATYPE}"
-	DEPOPULATE_ARGS="-action remove -removeFileName ${DEL_FILE} -dbString ${DB_STRING} -localModelName ${DB_REMOTE_MODEL} -password ${DB_PASSWORD} -userName ${DB_USERNAME} -jenaType ${DB_JENATYPE}"
+
+	POPULATE_ARGS="-action add -addFileName ${ADD_ABS_FILE} -dbString ${DB_STRING} -localModelName ${DB_REMOTE_MODEL} -password ${DB_PASSWORD} -userName ${DB_USERNAME} -jenaType ${DB_JENATYPE}"
+	DEPOPULATE_ARGS="-action remove -removeFileName ${DEL_ABS_FILE} -dbString ${DB_STRING} -localModelName ${DB_REMOTE_MODEL} -password ${DB_PASSWORD} -userName ${DB_USERNAME} -jenaType ${DB_JENATYPE}"
 	echo ${CURRENT_DATE} > ${FILE_LATEST}
 	LATEST_TS=$((${LATEST_TS} + ${DAYSEC}))
 
 	echo "Fetching add file"
-	wget -cnv ${ADD_URL}
+	wget -cnv ${ADD_URL} -O ${ADD_ABS_FILE}
 	RETURN=$?
 	if [ ${RETURN} -gt 0 ]
 	then
 		echo "Unable to fetch file: ${RETURN}"
 	else	
 		echo "Adding file"
-		clean_up_file ${ADD_FILE}
+		clean_up_file ${ADD_ABS_FILE}
 		java ${JAVA_ARGS} -jar VivoIngest.jar ${POPULATE_ARGS}
 		cat construct-add.ttl >> ${CURRENT_DATE}-construct-add.ttl
 		rm construct-add.ttl
 		cat construct-remove.ttl >> ${CURRENT_DATE}-construct-remove.ttl
 		rm construct-remove.ttl
-		bzip2 ${ADD_FILE}
+		bzip2 ${ADD_ABS_FILE}
 	fi
 
 	echo "Fetching del file"
-	wget -cnv ${DEL_URL}
+	wget -cnv ${DEL_URL} -O ${DEL_ABS_FILE}
 	RETURN=$?
 	if [ ${RETURN} -gt 0 ]
 	then
 		echo "Unable to fetch file: ${RETURN}"
 	else	
 		echo "Deleting file"
-		clean_up_file ${DEL_FILE}
+		clean_up_file ${DEL_ABS_FILE}
 		java ${JAVA_ARGS} -jar VivoIngest.jar ${DEPOPULATE_ARGS}
-		bzip2 ${DEL_FILE}
+		bzip2 ${DEL_ABS_FILE}
 	fi
 done
